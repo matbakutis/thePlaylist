27bc6ecadcc1cb06a96e615a67184a6fd4bb0e4b
sjgraffjr <sjgraffjr@gmail.com>
2017-10-21 10:59:10 -0400

    setup playlist controller and views

--- controllers/playlistController.js
+++ controllers/playlistController.js
@@ -6,7 +6,10 @@ const playlistDB = require('../models/playlistModel');
 
 router.route('/')
 	  .get((req, res)=>{
-	  	res.render('playlist/index', {});
+	  	playlistDB.find((err, playlists)=>{
+	  		res.render('playlist/index', {playlists: playlists});
+	  	})
+	  	
 	  });
 
 router.get('/new', (req,res)=>{
@@ -19,5 +22,45 @@ router.post('/',(req,res)=>{
 	})
 })
 
+router.get('/:id/edit', (req, res) => {
+  playlistDB.findById(req.params.id, (err, playlist) => {
+        res.render('playlist/edit', {playlist: playlist})
+      });
+    });
+                                                                                          
+router.route('/:id')
+  .get((req, res) => {
+    playlistDB.findById(req.params.id, (err, playlist) => {
+        res.render('playlist/show', {playlist: playlist })
+                                                 
+      })
+    })
+ 
+  .delete((req, res) => {
+    // if(req.session.logged === true){
 
+     playlistDB.findByIdAndRemove(req.params.id, (err, deletedPlaylist) => {
+        // req.params.id gives us the articles id
+                res.redirect('/playlists')
+          })
+    // } else {
+    //   // req.session.notLoggedMessage = 'Hey You gotta log in to delete an article, idiot';
+    //   res.redirect('/')
+    // // }
+
+    })
+
+  .put((req, res) => {
+   // if(req.session.logged === true){
+
+     playlistDB.findByIdAndUpdate(req.params.id, (err, updatedPlaylist) => {
+        // req.params.id gives us the articles id
+                res.redirect('/playlists')
+          })
+    // } else {
+    //   req.session.notLoggedMessage = 'Hey You gotta log in to delete an article, idiot';
+    //   res.redirect('/')
+    // }
+
+    })
 module.exports = router;
