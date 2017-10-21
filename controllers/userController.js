b29e63b30ee6f6df8c6a696843dd526f0ea70388
matbakutis <31048002+matbakutis@users.noreply.github.com>
2017-10-20 22:53:35 -0500

    intergrated sessions and sign in/sign out

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,17 +5,31 @@ const userDB = require('../models/userModel');
 
 router.route('/signup')
 	  .get((req, res)=>{
-	  	res.render('user/new', {});
+	  	res.render('user/new', {session: req.session});
 	  })
 	  .post((req, res)=>{
-	  	userDB.create({firstName: req.body.firstName, lastName: req.body.lastName, username: req.body.username, profilePic: req.body.profilePic}, (err, user)=>{
+	  	userDB.findOne({username: req.body.username}, (err, user)=>{
 	  		if (err) {
-	  			res.send('There was an error creating user.');
+	  			res.send('There was an error finding user.');
 	  		}else {
-	  			req.session.logged = true;
-				req.session.username = user.username;
-				console.log(user.profilePic.length)
-				res.redirect('/user/profile/' + user._id);
+	  			if (user) {
+	  				req.session.messageCreate = "Username Taken";
+	  				res.redirect('/user/signup');
+	  			}else {
+	  				userDB.create({firstName: req.body.firstName, lastName: req.body.lastName, username: req.body.username, profilePic: req.body.profilePic}, (err, user)=>{
+				  		if (err) {
+				  			res.send('There was an error creating user.');
+				  		}else {
+				  			req.session.logged = true;
+							req.session.username = user.username;
+							req.session.userId = user._id;
+							req.session.message = "";
+					 		req.session.messageCreate = "";
+					 		req.session.messageLogIn = "";
+							res.redirect('/user/profile/' + user._id);
+				  		};
+				  	});
+	  			};
 	  		};
 	  	});
 	  });
@@ -23,13 +37,22 @@ router.route('/signup')
 	  // password encrypt
 	  // , password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
 
+router.route('/profile/')
+	  .get((req, res)=>{
+	  	if (req.session.logged) {
+	  		res.redirect('/user/profile/' + req.session.userId);
+	  	}else {
+	  		res.redirect('/user/signup');
+	  	};
+	  })
+
 router.route('/profile/:id')
 	  .get((req, res)=>{
 	  	userDB.findById(req.params.id, (err, user)=>{
 		  	if (err) {
 		  		res.send('there was an error finding user');
 		  	}else {
-		  		res.render('user/profile', {user: user});
+		  		res.render('user/profile', {session: req.session, user: user});
 		  	};
 	  	});
 	  })
@@ -42,21 +65,30 @@ router.route('/login')
 		  			res.send('there was an error finding username');
 		  		}else {
 		  	// 		if (bcrypt.compareSync(req.body.password, user.password) === false) {
-					// 	req.session.message = 'Username or Password Incorrect';
+					// 	req.session.messageLogIn = 'Username or Password Incorrect';
 					//  res.redirect('/user/signup');
 					// }else if (bcrypt.compareSync(req.body.password, user.password) === true){
-					// 	req.session.logged = true;
-					// 	req.session.username = user.username;
-					//  req.session.message = "";
+						req.session.logged = true;
+						req.session.username = user.username;
+						req.session.userId = user._id;
+					 	req.session.message = "";
+					 	req.session.messageCreate = "";
+					 	req.session.messageLogIn = "";
 						res.redirect('/user/profile/' + user._id);
 					// };
 		  		};
 		  	}else {
-		  		req.session.message = 'Username or Password Incorrect';
+		  		req.session.messageLogIn = 'Username or Password Incorrect';
 				res.redirect('/user/signup');
 			};
 	  	});
 	  });
+
+router.route('/signout')
+	  .get((req, res)=>{
+	  	req.session.destroy();
+	  	res.redirect('/');
+	  })
 
 
 
